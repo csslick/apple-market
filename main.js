7538865734133881e58bea3f16eda8c7282cecd9
csslick <digitone@naver.com>
2021-06-21 00:04:42 +0900

    이미지 업로드

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const db = firebase.firestore();
 db.collection('product').get().then(res => {
   // DB 읽을때 권한이 필요함(규칙 if true 으로 수정)
   res.forEach(doc => {
-    console.log(doc.data())
+    // console.log(doc.data())
     const data = doc.data();
     appendHTML(data)
   })
@@ -12,14 +12,19 @@ db.collection('product').get().then(res => {
 
 function appendHTML(data) {
   const trade_date = data.date;
-  
+  let img_tag = '<img src="http://placehold.it/300x200">';
+  if(data.img_url) {
+    img_tag = `<img src="${data.img_url}">`
+  } 
   console.log(trade_date)
+
   const html = `
     <div class="product">
-      <img src="http://placehold.it/300x200" >
+      ${img_tag}
       <div class="info">
         <h4 class="title">${data.title}</h4>
         <p class="price">${data.price.toLocaleString()}원</p>
+        <p class="text">${data.text}</p>
         <p class="price">${trade_date}</p>
       </div>    
     </div>
